7ca4a8cf0a8a7d2caceb546bfc57353890ce3154
Martin Balverde <tinchoabalverde@gmail.com>
2023-02-01 10:53:12 +0100

    Se oculta numero de tarjeta

--- src/components/CreditCard/CreditCard.jsx
+++ src/components/CreditCard/CreditCard.jsx
@@ -6,13 +6,12 @@ import mastercard from '../../assets/mastercard.png'
 export default function CreditCard ({type,number,expirationMonth,expirationYear,bank,owner,cssClass}) {
 
 
-
   return (
     <>
     { 
     <div className={cssClass}>
         <img src={(type.toLowerCase() === 'visa' )? visa : mastercard} alt="{card.type}"></img>
-        <h3>{number}</h3>
+        <h3>**** **** **** {number.slice(number.length - 5)}</h3>
         <div className='expBank'>
             <p>Expires {expirationMonth}/{expirationYear}</p>
             <p>{bank}</p>
