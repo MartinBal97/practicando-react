f60dfb821eb1b8559fa13168783fd8e3b82e14d6
Martin Balverde <tinchoabalverde@gmail.com>
2023-02-01 10:23:04 +0100

    Enviando y trayendo datos desde App a otro componente e imprimiendo en el DOM

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import CreditCard from "./components/CreditCard/CreditCard";
 
 function App() {
+
+  const cards = [
+    {
+        type:'Visa',
+        number:'4569 7852 5687 8845',
+        expirationMonth: 3,
+        expirationYear: 21,
+        bank:'BNP',
+        owner:'Maxence Bouret',
+        cssClass:'card green'
+    },{
+        type:'Master Card',
+        number:'4569 7852 5687 0995',
+        expirationMonth: 3,
+        expirationYear: 21,
+        bank:'N26',
+        owner:'Jason Bourne',
+        cssClass:'card grey'
+    },{
+        type:'Visa',
+        number:'4569 7852 5687 6984',
+        expirationMonth: 12,
+        expirationYear: 19,
+        bank:'Santander Bank',
+        owner:'Alfredo Ramos',
+        cssClass:'card gold'
+    }
+    ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     {cards.map((card,i) => {
+        return <CreditCard type={card.type} number={card.number} expirationMonth={card.expirationMonth} expirationYear={card.expirationYear} bank={card.bank} owner={card.owner} cssClass={card.cssClass} key={i}/>
+     })}
+    </>
   );
 }
 
---
+++ src/components/CreditCard/CreditCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './CreditCard.css'
+import visa from '../../assets/visa.png'
+import mastercard from '../../assets/mastercard.png'
+
+export default function CreditCard ({type,number,expirationMonth,expirationYear,bank,owner,cssClass}) {
+
+
+
+  return (
+    <>
+    { 
+    <div className={cssClass}>
+        <img src={(type.toLowerCase() === 'visa' )? visa : mastercard} alt="{card.type}"></img>
+        <h3>{number}</h3>
+        <div className='expBank'>
+            <p>Expires {expirationMonth}/{expirationYear}</p>
+            <p>{bank}</p>
+        </div>
+        <p>{owner}</p>
+    </div>
+    } 
+    </>
+  );
+}
+
+
